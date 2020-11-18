3374ddbea71a9348782e893ec6d8644b2b4c3f8c
abhijeet2468 <abhijeet.pandey.98284@gmail.com>
2020-11-18 16:43:23 +0530

    Raptor header

---
+++ raptortheme/web/js/client.js
@@ -0,0 +1,10 @@
+// console.log("We are inside Jquery . Do what you want and bee happy and enjoy your life ");
+jQuery(document).ready(function(){
+    jQuery('#toggle').click(function(){
+      jQuery('.inside-catalogue').toggle('toggle-button');
+  
+    })
+    // We can write jQuery inside the .ready statement beacause it is neccessary
+   
+});
+
